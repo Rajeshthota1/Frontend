90cf32ab637f637939110b0f5fa509d5392f4126
THOTA RAJESH <114131408+Rajeshthota1@users.noreply.github.com>
2024-05-21 11:18:36 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,6 @@
+/* this is first code in js
+variable initialization*/
+a=10;
+b=5;
+c=10
+console.log(a+b+c);
---
+++ 1st.js
@@ -0,0 +1,3 @@
+console.log("Welcome to apna college");
+console.log("Hello World");
+console.log("I love JavaScript")
