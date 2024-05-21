4d3c692fec1169e0498de9b486213c947bfe33ac
THOTA RAJESH <114131408+Rajeshthota1@users.noreply.github.com>
2024-05-21 11:23:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+//getElementByid()
+
+console.log(document.getElementById("text-div"));
+
+
+//getElementByTagName()
+
+console.log(document.getElementsByTagName("h1"));
+console.log(document.getElementsByTagName("p"));
+
+// getElementByClassName()
+// const classItems = document.getElementsByClassName("content");
+// console.log(classItems);
+
+// to get para we use this
+const classItems = document.getElementsByClassName("text-para");
+console.log(classItems);
