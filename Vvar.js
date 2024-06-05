9f396d7c58b22540f2195c69f558effe6d574f8d
THOTA RAJESH <114131408+Rajeshthota1@users.noreply.github.com>
2024-06-05 15:46:11 +0530

    Add files via upload

---
+++ Vvar.js
@@ -0,0 +1,18 @@
+//LET VS CONST VS VAR
+
+//Using Var
+var someValue ="some data";
+someValue = "new value added for var";
+
+
+//using Let
+let firstName;
+firstName = "Thota Rajesh";
+
+//Using Const - we cannot re-assign
+const address="HYD"
+// address="Hyderabad"
+
+console.log(someValue);
+console.log(firstName);
+console.log(address);
